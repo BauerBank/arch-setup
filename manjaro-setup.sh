38ad491e921b9e32d39be2d02814222230fb645a
BauerBank <bauer.luca@t-online.de>
2021-11-16 23:51:57 +0100

    Update manjaro-setup.sh

--- manjaro-setup.sh
+++ manjaro-setup.sh
@@ -2,7 +2,7 @@
 #Update the Pc
 pacman -Syu
 #Install the esiacels from the offical repos
-pacman -S firefox thunderbird discord teamspeak3 git yay
+pacman -S firefox thunderbird discord teamspeak3 git yay nano
 #update the AUR
 yay -Syu
 #install packegs from the AUR
