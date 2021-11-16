a21e26c0808b2b66567a87878f1ccc2412beba77
BauerBank <bauer.luca@t-online.de>
2021-11-16 18:05:54 +0100

    Update manjaro-setup.sh

--- manjaro-setup.sh
+++ manjaro-setup.sh
@@ -3,3 +3,11 @@
 pacman -Syu
 #Install the esiacels from the offical repos
 pacman -S firefox thunderbird discord teamspeak3 git
+#make the aur work and install yay
+git clone https://aur.archlinux.org/yay-git.git
+cd yay-git
+makepkg -si
+cd ~
+rm -d yay-git
+yay -Syu
+yay -S teams spotify visual-studio-code-bin github-desktop
