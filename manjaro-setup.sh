061d7edfdc2fdfdf7b5d6b4934b46de74d46fad4
BauerBank <94458982+BauerBank@users.noreply.github.com>
2021-11-17 10:50:52 +0100

    Update manjaro-setup.sh

--- manjaro-setup.sh
+++ manjaro-setup.sh
@@ -6,4 +6,4 @@ pacman -S firefox thunderbird discord teamspeak3 git yay nano
 #update the AUR
 yay -Syu
 #install packegs from the AUR
-yay -S teams spotify visual-studio-code-bin github-desktop
+yay -S teams spotify visual-studio-code-bin github-desktop whatsdesk-bin
